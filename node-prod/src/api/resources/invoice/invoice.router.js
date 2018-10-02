54561cdfa5db88031864310e53efd24ff994db94
Prashant Shukla <prashantcs0061@gmail.com>
2018-10-02 09:14:53 +0530

    chanegs

--- node-prod/src/api/resources/invoice/invoice.router.js
+++ node-prod/src/api/resources/invoice/invoice.router.js
@@ -9,9 +9,10 @@ invoiceRouter.route('/')
     .post(passport.authenticate('jwt', {session:false}), invoiceController.create)
     .get(passport.authenticate('jwt', {session:false}), invoiceController.findAll);
 
-invoiceRouter.route(':/id')
+invoiceRouter.route('/:id')
     .put(passport.authenticate('jwt', {session:false}), invoiceController.update)
     .delete(passport.authenticate('jwt', {session:false}), invoiceController.delete)
     .get(passport.authenticate('jwt', {session:false}), invoiceController.findOne);
 
+    
 
